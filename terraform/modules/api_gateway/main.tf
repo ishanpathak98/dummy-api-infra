f1f5257dfd706089a4b7174db1162be6b4469721
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2025-02-22 21:46:35 +0530

    Update main.tf

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -1,4 +1,40 @@
+# terraform/modules/api_gateway/main.tf
 resource "aws_api_gateway_rest_api" "main" {
-  name        = "example-api"
-  description = "Example API Gateway"
+  name = "${var.project_name}-api"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+  tags = var.tags
 }
+
+resource "aws_api_gateway_resource" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "main" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.main.id
+  http_method   = "ANY"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.main.id
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.main.id
+  http_method = aws_api_gateway_method.main.http_method
+
+  integration_http_method = "POST"
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${var.alb_dns_name}/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.
