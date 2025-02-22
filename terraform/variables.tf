0affaa443b60a9ba9fb5f05899845aa3aed3b82c
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2025-02-22 21:38:01 +0530

    Update variables.tf
    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,27 @@
-variable "region" {
+variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "dummy-data-generator"
+}
+
+variable "tags" {
+  description = "Common tags for all resources"
+  type        = map(string)
+  default = {
+    Project     = "Dummy Data Generator"
+    Environment = "Production"
+    Terraform   = "true"
+  }
 }
