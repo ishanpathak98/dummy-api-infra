05564d7d6b7cc6fa3b4a69e51b8fc103384775e7
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2025-02-22 21:49:05 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,4 @@
+# terraform/provider.tf
 provider "aws" {
   region = var.aws_region
 }
@@ -14,5 +15,34 @@ terraform {
     bucket = "terraform-state-dummy-data-generator"
     key    = "terraform.tfstate"
     region = "us-west-2"
+  }
+}
+
+# terraform/variables.tf
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "dummy-data-generator"
+}
+
+variable "tags" {
+  description = "Common tags for all resources"
+  type        = map(string)
+  default = {
+    Project     = "Dummy Data Generator"
+    Environment = "Production"
+    Terraform   = "true"
   }
 }
