5f14e30956f7be435c86d7d907c1677e3b311a31
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2025-02-22 21:37:07 +0530

    Update provider.tf
    Updated provide.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,18 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
+  }
+  backend "s3" {
+    bucket = "terraform-state-dummy-data-generator"
+    key    = "terraform.tfstate"
+    region = "us-west-2"
   }
 }
