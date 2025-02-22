34d089f161ee12f9202f76536253d9071b86e37b
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2025-02-22 21:54:03 +0530

    Update main.tf
    monitoring

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -1,3 +1,98 @@
-resource "aws_cloudwatch_log_group" "main" {
-  name = "example-log-group"
+# modules/monitoring/main.tf
+
+resource "aws_cloudwatch_log_group" "prometheus_logs" {
+  name              = "/ecs/prometheus"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "grafana_logs" {
+  name              = "/ecs/grafana"
+  retention_in_days = 7
+}
+
+resource "aws_ecs_task_definition" "prometheus" {
+  family                   = "prometheus"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "prometheus"
+      image = "prom/prometheus:latest"
+      portMappings = [
+        {
+          containerPort = 9090
+          hostPort      = 9090
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.prometheus_logs.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "grafana" {
+  family                   = "grafana"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([
+    {
+      name  = "grafana"
+      image = "grafana/grafana:latest"
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+      environment = [
+        { name = "GF_SECURITY_ADMIN_PASSWORD", value = var.grafana_admin_password }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.grafana_logs.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "prometheus" {
+  name            = "prometheus-service"
+  cluster        = aws_ecs_cluster.monitoring.id
+  task_definition = aws_ecs_task_definition.prometheus.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = var.subnets
+    security_groups = var.security_groups
+  }
+}
+
+resource "aws_ecs_service" "grafana" {
+  name            = "grafana-service"
+  cluster        = aws_ecs_cluster.monitoring.id
+  task_definition = aws_ecs_task_definition.grafana.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = var.subnets
+    security_groups = var.security_groups
+  }
 }
